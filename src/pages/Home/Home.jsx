e00077b1b1a70c4d8e45fce88a42e3bb0109764d
Kanay <kanosss1@gmail.com>
2023-08-31 22:28:24 +0600

    gitlab

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,56 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import home from "./Home.module.css";
-import axios from "axios";
-import { MockAPI } from "../../utils/const";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  const [data, setData] = useState([]);
-  const dayOfWeek = [
-    "Понедельник",
-    "Вторник",
-    "Среда",
-    "Четверг",
-    "Пятница",
-    "Суббота",
-  ];
-
-  const getSchedule = async () => {
-    try {
-      const res = await axios.get(MockAPI);
-      setData(res.data);
-      console.log(data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  useEffect(() => {
-    getSchedule();
-  }, []);
-
   return (
     <>
-      <div className={home.section}>
-        <div className={home.container}>
-          <h2 className={home.txt}>Расписание учебного заведение</h2>
-          <div className={home.schedule}>
-            {dayOfWeek.map((day) => (
-              <div className={home.list} key={day}>
-                <div className={home.item}>{day}</div>
-                {data
-                  .filter((item) => item.day === day)
-                  .map((item) => (
-                    <ul key={item.id}>
-                      <li>{item.course}</li>
-                      <li>{item.time}</li>
-                      <li>{item.mentor}</li>
-                    </ul>
-                  ))}
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
+      <Link to="/main">
+        <button>Main</button>
+      </Link>
+      <Link to="/design1">
+        <button>DESIGN 1</button>
+      </Link>
+      <Link to="/design2">
+        <button>DESIGN 2</button>
+      </Link>
+      <Link to="/design3">
+        <button>DESIGN 3</button>
+      </Link>
     </>
   );
 };
